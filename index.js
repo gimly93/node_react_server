d2ab9cb81d927f42bf68de8f334f3672a175a799
gimly93 <Arsenal1!!>
2017-11-14 17:04:30 +0300

    client

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 require('./models/user');
 require('./services/passport');
-console.log(keys);
 mongoose.connect(keys.mongoURI);
 
 const app = express();
