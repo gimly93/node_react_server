61aa594192e2be61a68b10e837193f500c6aeb57
gimly93 <a.lukashevich93@gmail.com>
2017-11-16 00:34:27 +0300

    redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './Header';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
 
 const Dashboard = () => {
-    return ( <h2>Dashboard</h2>)
+  return ( <h2>Dashboard</h2>)
 };
 
 const SurveyNew = () => {
-    return (<h2>SurvayNew</h2>)
+  return (<h2>SurvayNew</h2>)
 };
 
 const Landing = () => {
-    return (<h2>Landing</h2>)
+  return (<h2>Landing</h2>)
 };
 
-const App = () => {
+class App extends Component {
+  componentDidMount(){
+    this.props.fetchUser();
+  }
+
+  render() {
     return (
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Header/>
-                    <Route path="/" exact component={Landing}/>
-                    <Route path="/surveys" exact component={Dashboard}/>
-                    <Route path="/surveys/new"  component={SurveyNew}/>
-                </div>
-            </BrowserRouter>
-        </div>
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header/>
+            <Route path="/" exact component={Landing}/>
+            <Route path="/surveys" exact component={Dashboard}/>
+            <Route path="/surveys/new" component={SurveyNew}/>
+          </div>
+        </BrowserRouter>
+      </div>
     )
+  }
+
 };
-export default App;
+
+export default connect(null, actions)(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
-
+import Link from 'react-router-dom';
 class Header extends Component{
     render(){
         return (
-            <div>
-                Header
-            </div>
+          <nav>
+              <div className="nav-wrapper">
+                  <a href="/" className="brand-logo">Emaily</a>
+                  <ul id="nav-mobile" className="right">
+                      <li><a href="/auth/google">Login with Google</a></li>
+                  </ul>
+              </div>
+          </nav>
         )
     }
 }
