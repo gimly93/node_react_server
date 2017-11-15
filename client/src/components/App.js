d039c210cf4519078840756669b8be9fc33101fe
gimly93 <Arsenal1!!>
2017-11-15 16:34:31 +0300

    components + materialize

---
+++ client/src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Header from './Header';
+
+const Dashboard = () => {
+    return ( <h2>Dashboard</h2>)
+};
+
+const SurveyNew = () => {
+    return (<h2>SurvayNew</h2>)
+};
+
+const Landing = () => {
+    return (<h2>Landing</h2>)
+};
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route path="/" exact component={Landing}/>
+                    <Route path="/surveys" exact component={Dashboard}/>
+                    <Route path="/surveys/new"  component={SurveyNew}/>
+                </div>
+            </BrowserRouter>
+        </div>
+    )
+};
+export default App;
